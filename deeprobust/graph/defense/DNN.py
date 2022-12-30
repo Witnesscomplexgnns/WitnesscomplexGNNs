463d21f9a50afc491415b679fab036a3db541f51
Yuzhou Chen <yuzhouchen888@gmail.com>
2022-12-30 00:47:04 -0500

    Witness complex-based GNNs

---
+++ deeprobust/graph/defense/DNN.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+
+# DNN
+class CNN(nn.Module):
+    def __init__(self, dim_hidden, dim_out):
+        super(CNN, self).__init__()
+        self.dim_out = dim_out
+        self.features = nn.Sequential(
+            nn.Conv2d(1, dim_hidden, kernel_size=3, stride=2), #channel of witness_complex_topo is 1
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(dim_hidden, dim_out, kernel_size=3, stride=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.maxpool = nn.MaxPool2d(3, 3)
+
+    def forward(self, witness_complex_topo):
+        feature = self.features(witness_complex_topo)
+        feature = self.maxpool(feature)
+        feature = feature.view(-1, self.dim_out) #B, dim_out
+        return feature
