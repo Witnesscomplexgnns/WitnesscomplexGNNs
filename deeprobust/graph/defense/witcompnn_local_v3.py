05b77b037f1bd739968f5ab70d799f59ceed10cb
Naheed <naheedcse@gmail.com>
2023-09-07 19:23:54 +0800

    models for Local PI

---
+++ deeprobust/graph/defense/witcompnn_local_v3.py
@@ -0,0 +1,327 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+import torch
+import torch.optim as optim
+from torch.nn.parameter import Parameter
+from torch.nn.modules.module import Module
+from deeprobust.graph import utils
+from copy import deepcopy
+from sklearn.metrics import f1_score
+import torchvision.models as models
+import numpy as np
+from vit_pytorch import ViT
+# https://github.com/lucidrains/vit-pytorch#vision-transformer---pytorch
+# pip install vit-pytorch
+
+class Attention(nn.Module):
+    def __init__(self, in_size, hidden_size=32):
+        super(Attention, self).__init__()
+
+        self.project = nn.Sequential(
+            nn.Linear(in_size, hidden_size),
+            nn.Tanh(),
+            nn.Linear(hidden_size, 1, bias=False)
+        )
+
+    def forward(self, z):
+        w = self.project(z)
+        beta = torch.softmax(w, dim=1)
+        return (beta * z).sum(1), beta
+
+
+class GraphConvolution(Module):
+    """Simple GCN layer, similar to https://github.com/tkipf/pygcn
+    """
+
+    def __init__(self, in_features, out_features, with_bias=True):
+        super(GraphConvolution, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
+        if with_bias:
+            self.bias = Parameter(torch.FloatTensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias is not None:
+            self.bias.data.uniform_(-stdv, stdv)
+
+    def forward(self, input, adj):
+        """ Graph Convolutional Layer forward function
+        """
+        if input.data.is_sparse:
+            support = torch.spmm(input, self.weight)
+        else:
+            support = torch.mm(input, self.weight)
+        output = torch.spmm(adj, support)
+        if self.bias is not None:
+            return output + self.bias
+        else:
+            return output
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' \
+               + str(self.in_features) + ' -> ' \
+               + str(self.out_features) + ')'
+
+
+class LWitCompNN_V3(nn.Module):
+    """ 2 Layer Graph Convolutional Network + witness complex-based layer
+    """
+    def __init__(self, nfeat, nhid, nclass, dropout=0.5, lr=0.01, weight_decay=5e-4,
+            with_relu=True, with_bias=True, device=None, alpha = 0.5, beta = 0.5, aggregation_method = 'attention'):
+
+        super(LWitCompNN_V3, self).__init__()
+
+        assert device is not None, "Please specify 'device'!"
+        self.device = device
+        self.nfeat = nfeat
+        self.hidden_sizes = [nhid]
+        self.nclass = nclass
+        self.cnn = ViT(
+                image_size = 50,
+                patch_size = 25,
+                num_classes = nhid,
+                dim = 32, #1024,
+                depth = 6,
+                heads = 16,
+                mlp_dim = 64, #2048,
+                dropout = 0.1,
+                channels=1, # channel is 1
+                emb_dropout = 0.1
+                )
+        self.attention = Attention(nhid)
+        self.gc1 = GraphConvolution(nfeat, nhid, with_bias=with_bias)
+        self.gc2 = GraphConvolution(nhid, nclass, with_bias=with_bias)
+        self.dropout = dropout
+        self.lr = lr
+        if not with_relu:
+            self.weight_decay = 0
+        else:
+            self.weight_decay = weight_decay
+        self.with_relu = with_relu
+        self.with_bias = with_bias
+        self.output = None
+        self.best_model = None
+        self.best_output = None
+        self.adj_norm = None
+        self.features = None
+        self.agg = aggregation_method
+        self.alpha = alpha
+        self.beta = beta
+
+    def forward(self, x, adj, witness_complex_feat):
+        if self.with_relu:
+            x = F.relu(self.gc1(x, adj))
+        else:
+            x = self.gc1(x, adj)
+
+        witness_comp_topo = self.cnn(witness_complex_feat)
+        # x and witness_comp_topo have the same size
+        if self.agg == 'einsum':
+            x = torch.einsum('nb, nb-> nb', x, witness_comp_topo)
+        elif self.agg == 'weighted_sum':
+            x = self.alpha * x + self.beta * witness_comp_topo
+        elif self.agg == 'attention':
+            emb = torch.stack([x, witness_comp_topo], dim=1)
+            emb, att = self.attention(emb)
+            x = emb
+
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = self.gc2(x, adj)
+        return F.log_softmax(x, dim=1)
+
+    def initialize(self):
+        """Initialize parameters of GCN.
+        """
+        self.gc1.reset_parameters()
+        self.gc2.reset_parameters()
+
+    def fit(self, features, adj, witness_complex_feat, labels, idx_train, idx_val=None, train_iters=200, initialize=True, verbose=False, normalize=True, patience=500, **kwargs):
+        self.device = self.gc1.weight.device
+        if initialize:
+            self.initialize()
+
+        if type(adj) is not torch.Tensor:
+            features, adj, labels = utils.to_tensor(features, adj, labels, device=self.device)
+        else:
+            features = features.to(self.device)
+            adj = adj.to(self.device)
+            labels = labels.to(self.device)
+
+        if normalize:
+            if utils.is_sparse_tensor(adj):
+                adj_norm = utils.normalize_adj_tensor(adj, sparse=True)
+            else:
+                adj_norm = utils.normalize_adj_tensor(adj)
+        else:
+            adj_norm = adj
+
+        self.adj_norm = adj_norm
+        self.features = features.to(self.device)
+        self.labels = labels
+        self.witness_complex_feat = witness_complex_feat.to(self.device)
+
+        if idx_val is None:
+            self._train_without_val(labels, idx_train, train_iters, verbose)
+        else:
+            if patience < train_iters:
+                self._train_with_early_stopping(labels, idx_train, idx_val, train_iters, patience, verbose)
+            else:
+                self._train_with_val(labels, idx_train, idx_val, train_iters, verbose)
+
+    def _train_without_val(self, labels, idx_train, train_iters, verbose):
+        self.train()
+        optimizer = optim.Adam(self.parameters(), lr=self.lr, weight_decay=self.weight_decay)
+        for i in range(train_iters):
+            optimizer.zero_grad()
+            output = self.forward(self.features.to(self.device), self.adj_norm, self.witness_complex_feat.to(self.device))
+            loss_train = F.nll_loss(output[idx_train], labels[idx_train])
+            loss_train.backward()
+            optimizer.step()
+            if verbose and i % 1 == 0:
+                print('Epoch {}, training loss: {}'.format(i, loss_train.item()))
+
+        self.eval()
+        output = self.forward(self.features.to(self.device), self.adj_norm, self.witness_complex_feat.to(self.device))
+        self.output = output
+
+    def _train_with_val(self, labels, idx_train, idx_val, train_iters, verbose):
+        if verbose:
+            print('=== training gcn model ===')
+        optimizer = optim.Adam(self.parameters(), lr=self.lr, weight_decay=self.weight_decay)
+
+        best_loss_val = 100
+        best_acc_val = 0
+
+        for i in range(train_iters):
+            self.train()
+            optimizer.zero_grad()
+            output = self.forward(self.features, self.adj_norm, self.witness_complex_feat)
+            loss_train = F.nll_loss(output[idx_train], labels[idx_train])
+            loss_train.backward()
+            optimizer.step()
+
+            if verbose and i % 1 == 0:
+                print('Epoch {}, training loss: {}'.format(i, loss_train.item()))
+
+            self.eval()
+            output = self.forward(self.features, self.adj_norm, self.witness_complex_feat)
+            loss_val = F.nll_loss(output[idx_val], labels[idx_val])
+            acc_val = utils.accuracy(output[idx_val], labels[idx_val])
+
+            if best_loss_val > loss_val:
+                best_loss_val = loss_val
+                self.output = output
+                weights = deepcopy(self.state_dict())
+
+            if acc_val > best_acc_val:
+                best_acc_val = acc_val
+                self.output = output
+                weights = deepcopy(self.state_dict())
+
+        if verbose:
+            print('=== picking the best model according to the performance on validation ===')
+        self.load_state_dict(weights)
+
+    def _train_with_early_stopping(self, labels, idx_train, idx_val, train_iters, patience, verbose):
+        if verbose:
+            print('=== training gcn model ===')
+        optimizer = optim.Adam(self.parameters(), lr=self.lr, weight_decay=self.weight_decay)
+
+        early_stopping = patience
+        best_loss_val = 100
+
+        for i in range(train_iters):
+            self.train()
+            optimizer.zero_grad()
+            output = self.forward(self.features, self.adj_norm, self.witness_complex_feat)
+            loss_train = F.nll_loss(output[idx_train], labels[idx_train])
+            loss_train.backward()
+            optimizer.step()
+
+            if verbose and i % 1 == 0:
+                print('Epoch {}, training loss: {}'.format(i, loss_train.item()))
+
+            self.eval()
+            output = self.forward(self.features, self.adj_norm, self.witness_complex_feat)
+
+            # def eval_class(output, labels):
+            #     preds = output.max(1)[1].type_as(labels)
+            #     return f1_score(labels.cpu().numpy(), preds.cpu().numpy(), average='micro') + \
+            #         f1_score(labels.cpu().numpy(), preds.cpu().numpy(), average='macro')
+
+            # perf_sum = eval_class(output[idx_val], labels[idx_val])
+            loss_val = F.nll_loss(output[idx_val], labels[idx_val])
+
+            if best_loss_val > loss_val:
+                best_loss_val = loss_val
+                self.output = output
+                weights = deepcopy(self.state_dict())
+                patience = early_stopping
+            else:
+                patience -= 1
+            if i > early_stopping and patience <= 0:
+                break
+
+        if verbose:
+             print('=== early stopping at {0}, loss_val = {1} ==='.format(i, best_loss_val) )
+        self.load_state_dict(weights)
+
+    def test(self, idx_test):
+        """Evaluate GCN performance on test set.
+
+        Parameters
+        ----------
+        idx_test :
+            node testing indices
+        """
+        self.eval()
+        output = self.predict()
+        # output = self.output
+        loss_test = F.nll_loss(output[idx_test], self.labels[idx_test])
+        acc_test = utils.accuracy(output[idx_test], self.labels[idx_test])
+        print("Test set results:",
+              "loss= {:.4f}".format(loss_test.item()),
+              "accuracy= {:.4f}".format(acc_test.item()))
+        return acc_test.item()
+
+
+    def predict(self, features=None, adj=None):
+        """By default, the inputs should be unnormalized adjacency
+
+        Parameters
+        ----------
+        features :
+            node features. If `features` and `adj` are not given, this function will use previous stored `features` and `adj` from training to make predictions.
+        adj :
+            adjcency matrix. If `features` and `adj` are not given, this function will use previous stored `features` and `adj` from training to make predictions.
+
+
+        Returns
+        -------
+        torch.FloatTensor
+            output (log probabilities) of GCN
+        """
+
+        self.eval()
+        if features is None and adj is None:
+            return self.forward(self.features, self.adj_norm, self.witness_complex_feat)
+        else:
+            if type(adj) is not torch.Tensor:
+                features, adj = utils.to_tensor(features, adj, device=self.device)
+
+            self.features = features
+            if utils.is_sparse_tensor(adj):
+                self.adj_norm = utils.normalize_adj_tensor(adj, sparse=True)
+            else:
+                self.adj_norm = utils.normalize_adj_tensor(adj)
+            return self.forward(self.features, self.adj_norm, self.witness_complex_feat)
+
+
+
---
+++ witcompnn_main.py
@@ -0,0 +1,94 @@
+import torch
+import numpy as np
+import torch.nn.functional as F
+from deeprobust.graph.defense import GCN, GWitCompNN, LWitCompNN, LWitCompNN_V1, LWitCompNN_V2, LWitCompNN_V3
+from deeprobust.graph.utils import *
+from deeprobust.graph.data import Dataset
+import scipy.sparse as sp
+import argparse
+from utils import load_npz
+#import gudhi as gd
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--seed', type=int, default=15, help='Random seed.')
+parser.add_argument('--dataset', type=str, default='cora', choices=['cora'], help='dataset')
+parser.add_argument('--lr', type=float, default=0.01,  help='learning rate')
+parser.add_argument('--drop_rate', type=float, default=0.5,  help='dropout rate')
+parser.add_argument('--weight_decay', type=float, default=5e-4,  help='weight decay rate')
+parser.add_argument('--ptb_rate', type=float, default=0.05,  help='pertubation rate')
+parser.add_argument('--epoch', type=float, default=500,  help='epochs')
+parser.add_argument('--alpha', type=float, default=0.3,  help='alpha')
+parser.add_argument('--beta', type=float, default=0.7,  help='beta')
+parser.add_argument('--topo', type=str,default = 'witptb_local',help='witorig/witptb/witptb_local/vrorig/vrptb')
+
+args = parser.parse_args()
+args.cuda = torch.cuda.is_available()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# load original data and attacked data
+adj, features, labels = load_npz('data/' + args.dataset + '/' + args.dataset + '.npz')
+f = open('data/' + args.dataset + '/' + args.dataset + '_prognn_splits.json')
+idx = json.load(f)
+idx_train, idx_val, idx_test = np.array(idx['idx_train']), np.array(idx['idx_val']), np.array(idx['idx_test'])
+perturbed_adj = sp.load_npz('data/' + args.dataset + '/' + args.dataset + '_meta_adj_'+str(args.ptb_rate)+'.npz')
+
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+# Setup WitCompNN Model        
+# load witness complex topological features
+if args.topo == 'witorig':
+    witness_complex_feat = torch.FloatTensor(np.load('data/' + args.dataset + '/' + args.dataset + '_PI' + '.npz', allow_pickle=True)['arr_0'])
+
+if args.topo == 'witptb':
+    witness_complex_feat = torch.FloatTensor(np.random.rand(1,50,50)) #torch.FloatTensor(np.load('data/' + args.dataset + '/' + args.dataset + '_'+str(args.ptb_rate)+'_PI' + '.npz', allow_pickle=True)['arr_0'])
+    print('shape of local PI representation: ',witness_complex_feat.shape)
+
+if args.topo == 'witptb_local':
+    # local_witness_complex_feat => Shape (#nodes x 50 x 50)
+    local_witness_complex_feat_ = np.random.rand(2485, 50, 50) # torch.FloatTensor(np.load('data/' + args.dataset + '/' + args.dataset + '_'+str(args.ptb_rate)+'_localPI' + '.npz', allow_pickle=True)['arr_0'])
+    local_witness_complex_feat_ = np.expand_dims(local_witness_complex_feat_, axis=1)  # (#nodes, 1, pi_dim, pi_dim)
+    local_witness_complex_feat = torch.FloatTensor(local_witness_complex_feat_)
+    print('shape of local PI representation: ',local_witness_complex_feat.shape) # (#nodes, 1, pi_dim, pi_dim)
+
+
+topo_type = 'local' # 'local', 'global'
+method = 'transformer' # resnet, cnn, transformer
+aggregation_method = 'weighted_sum' # einsum, weighted_sum, attention
+
+if topo_type == 'global':
+    model = GWitCompNN(nfeat=features.shape[1], nhid=16, nclass=int(labels.max()) + 1, dropout=args.drop_rate,  lr=args.lr, weight_decay=args.weight_decay, device=device)
+    model = model.to(device)
+    model.fit(features, perturbed_adj, witness_complex_feat, labels, idx_train, train_iters=args.epoch, verbose=True)
+else:
+    if method == 'resnet':
+        print("You are using ResNet now!")
+        model = LWitCompNN_V1(nfeat=features.shape[1], nhid=16, nclass=int(labels.max()) + 1, dropout=args.drop_rate, lr=args.lr, weight_decay=args.weight_decay, device=device, alpha = args.alpha, beta = args.beta, aggregation_method = aggregation_method)
+        model = model.to(device)
+        model.fit(features, perturbed_adj, local_witness_complex_feat, labels, idx_train, train_iters=args.epoch,  verbose=True)
+    elif method == 'cnn':
+        print("You are using CNN now!")
+        model = LWitCompNN_V2(nfeat=features.shape[1], nhid=16, nclass=int(labels.max()) + 1, dropout=args.drop_rate, lr=args.lr, weight_decay=args.weight_decay, device=device, alpha = args.alpha, beta = args.beta, aggregation_method = aggregation_method)
+        model = model.to(device)
+        model.fit(features, perturbed_adj, local_witness_complex_feat, labels, idx_train, train_iters=args.epoch, verbose=True)
+    elif method == 'transformer':
+        print("You are using Transformer now!")
+        model = LWitCompNN_V3(nfeat=features.shape[1], nhid=16, nclass=int(labels.max()) + 1, dropout=args.drop_rate, lr=args.lr, weight_decay=args.weight_decay, device=device, alpha = args.alpha, beta = args.beta, aggregation_method = aggregation_method)
+        model = model.to(device)
+        model.fit(features, perturbed_adj, local_witness_complex_feat, labels, idx_train, train_iters=args.epoch, verbose=True)
+
+# # using validation to pick model
+# model.fit(features, perturbed_adj, labels, idx_train, idx_val, train_iters=200, verbose=True)
+model.eval()
+# You can use the inner function of model to test
+model.test(idx_test)
+
+
+
+
+
+# local_witness_complex_feat_ = np.expand_dims(local_witness_complex_feat_, axis=1)  # (#nodes, 1, pi_dim, pi_dim)
+# local_witness_complex_feat = np.tile(local_witness_complex_feat_, (1, 3, 1, 1))  # (#nodes, 3, pi_dim, pi_dim) which can be fed into resnet
+# local_witness_complex_feat = torch.FloatTensor(local_witness_complex_feat)
